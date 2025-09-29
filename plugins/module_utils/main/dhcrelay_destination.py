6556ccfbdfc420d45801082f91b5d6e266d2ef8b
Marius Rieder <marius.rieder@durchmesser.ch>
2025-09-29 16:53:07 +0200

    Implement mass management for dhcrelay_destination

--- plugins/module_utils/main/dhcrelay_destination.py
+++ plugins/module_utils/main/dhcrelay_destination.py
@@ -28,8 +28,11 @@ class DhcRelayDestination(BaseModule):
     }
     EXIST_ATTR = 'destination'
 
-    def __init__(self, module: AnsibleModule, result: dict, session: Session = None, fail: dict = None):
-        BaseModule.__init__(self=self, m=module, r=result, s=session, f=fail)
+    def __init__(
+            self, module: AnsibleModule, result: dict, multi: dict = None,
+            session: Session = None, fail: dict = None,
+    ):
+        BaseModule.__init__(self=self, m=module, r=result, s=session, f=fail, multi=multi)
         self.destination = {}
 
     def check(self) -> None:
--- plugins/modules/dhcrelay_destination.py
+++ plugins/modules/dhcrelay_destination.py
@@ -12,7 +12,10 @@ from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.handler impor
     module_dependency_error, MODULE_EXCEPTIONS
 
 try:
-    from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.wrapper import module_wrapper
+    from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.wrapper import \
+        module_wrapper, is_multi_module_call, module_multi_wrapper
+    from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.multi import \
+        build_multi_mod_args
     from ansible_collections.oxlorg.opnsense.plugins.module_utils.defaults.main import \
         OPN_MOD_ARGS, STATE_ONLY_MOD_ARG, RELOAD_MOD_ARG
     from ansible_collections.oxlorg.opnsense.plugins.module_utils.main.dhcrelay_destination import \
@@ -27,7 +30,7 @@ except MODULE_EXCEPTIONS:
 
 
 def run_module():
-    module_args = dict(
+    entry_args = dict(
         name=dict(
             type='str', required=True,
             description='A unique name for this relay destination.',
@@ -36,9 +39,19 @@ def run_module():
             type='list', elements='str', required=False,
             description='A list of server IP addresses to relay DHCP requests to.'
         ),
-        **RELOAD_MOD_ARG,
         **STATE_ONLY_MOD_ARG,
+    )
+    entry_multi_args = build_multi_mod_args(
+        mod_args=entry_args,
+        aliases=['dhcrelay_destinations', 'destinations'],
+        not_required=['name'],
+    )
+
+    module_args = dict(
+        **entry_args,
+        **entry_multi_args,
         **OPN_MOD_ARGS,
+        **RELOAD_MOD_ARG,
     )
 
     result = dict(
@@ -52,9 +65,26 @@ def run_module():
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True,
+        mutually_exclusive=[
+            ('name', 'multi'), ('name', 'multi_purge'), ('name', 'multi_control.purge_all')
+        ],
+        required_one_of=[
+            ('name', 'multi', 'multi_purge', 'multi_control.purge_all'),
+        ],
     )
 
-    module_wrapper(DhcRelayDestination(module=module, result=result))
+    if is_multi_module_call(module):
+        module_multi_wrapper(
+            module=module,
+            result=result,
+            obj=DhcRelayDestination,
+            kind='dhcrelay_destination',
+            entry_args=entry_multi_args,
+        )
+
+    else:
+        module_wrapper(DhcRelayDestination(module=module, result=result))
+
     module.exit_json(**result)
 
 
