6ec22565af6b69513e35cf465bc5c0488e560c74
Marius Rieder <marius.rieder@durchmesser.ch>
2025-10-01 17:13:09 +0200

    Implement mass management for dnsmasq_tag

--- plugins/module_utils/main/dnsmasq_tag.py
+++ plugins/module_utils/main/dnsmasq_tag.py
@@ -24,6 +24,9 @@ class Tag(BaseModule):
         'tag': r'^[0-9a-zA-Z]{1,1024}$'
     }
 
-    def __init__(self, module: AnsibleModule, result: dict, session: Session = None):
-        BaseModule.__init__(self=self, m=module, r=result, s=session)
+    def __init__(
+            self, module: AnsibleModule, result: dict, multi: dict = None,
+            session: Session = None, fail: dict = None,
+    ):
+        BaseModule.__init__(self=self, m=module, r=result, s=session, f=fail, multi=multi)
         self.tag = {}
--- plugins/modules/dnsmasq_tag.py
+++ plugins/modules/dnsmasq_tag.py
@@ -12,7 +12,10 @@ from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.handler impor
     module_dependency_error, MODULE_EXCEPTIONS
 
 try:
-    from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.wrapper import module_wrapper
+    from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.wrapper import \
+        module_wrapper, is_multi_module_call, module_multi_wrapper
+    from ansible_collections.oxlorg.opnsense.plugins.module_utils.base.multi import \
+        build_multi_mod_args
     from ansible_collections.oxlorg.opnsense.plugins.module_utils.defaults.main import \
         OPN_MOD_ARGS, STATE_ONLY_MOD_ARG
     from ansible_collections.oxlorg.opnsense.plugins.module_utils.main.dnsmasq_tag import Tag
@@ -26,12 +29,22 @@ except MODULE_EXCEPTIONS:
 
 
 def run_module():
-    module_args = dict(
+    entry_args = dict(
         tag=dict(
             type='str', required=True, aliases=['t', 'name', 'n'],
             description='DHCP tag.',
         ),
         **STATE_ONLY_MOD_ARG,
+    )
+    entry_multi_args = build_multi_mod_args(
+        mod_args=entry_args,
+        aliases=['dnsmasq_tags', 'tags'],
+        not_required=['tag'],
+    )
+
+    module_args = dict(
+        **entry_args,
+        **entry_multi_args,
         **OPN_MOD_ARGS,
     )
 
@@ -46,9 +59,25 @@ def run_module():
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True,
+        mutually_exclusive=[
+            ('tag', 'multi'), ('tag', 'multi_purge'), ('tag', 'multi_control.purge_all'),
+        ],
+        required_one_of=[
+            ('tag', 'multi', 'multi_purge', 'multi_control.purge_all'),
+        ],
     )
 
-    module_wrapper(Tag(module=module, result=result))
+    if is_multi_module_call(module):
+        module_multi_wrapper(
+            module=module,
+            result=result,
+            obj=Tag,
+            kind='dnsmasq_tag',
+            entry_args=entry_multi_args,
+        )
+
+    else:
+        module_wrapper(Tag(module=module, result=result))
     module.exit_json(**result)
 
 
